b7d06e7495300f8d8161393056c08577889e8a93
namrahov <nurlan.amrahov@pashabank.az>
2021-08-08 10:40:16 +0400

    etet

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/namrahov/banking
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"github.com/namrahov/banking/tree/master/app"
+	"github.com/namrahov/banking/app"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	//mux := NewHandlerMux()
+
 	http.HandleFunc("/greet", app.Greet)
-	http.HandleFunc("/customers", GetAllCustomers)
+	http.HandleFunc("/customers", app.GetAllCustomers)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
