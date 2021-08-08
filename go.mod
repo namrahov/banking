6740708023d1ede069940ccfe79caf953c865451
namrahov <nurlan.amrahov@pashabank.az>
2021-08-08 10:32:42 +0400

    sdfds

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/namrahov/banking
+
+go 1.16
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"github.com/namrahov/banking/tree/master/app"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/greet", Greet)
+	http.HandleFunc("/greet", app.Greet)
 	http.HandleFunc("/customers", GetAllCustomers)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
